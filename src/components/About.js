b4eaf6e0009b9c2708998e52d548ed9c4cd16e8c
swallehrashid <swallehmtawazo@gmail.com>
2025-10-31 13:40:25 +0300

    adds links to the web page

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,20 @@
-import React from "react";
 
-function About() {
+
+import React from "react";
+import Links from "./Links"; 
+
+function About({ bio, links }) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+    
+      {bio ? <p>{bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      
+      <Links github={links.github} linkedin={links.linkedin} />
     </div>
   );
 }
 
 export default About;
+
